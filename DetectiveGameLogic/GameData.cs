ce64d2a0020f83d0c085812ada4febc368185ce3
vlnt sl <orlovadd06@gmail.com>
2026-04-18 15:11:01 +0300

    Удалени классов SaveState, StateManager. Добавление других классов, заменяющих прошлые, GameData и Player.

---
+++ DetectiveGameLogic/GameData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBasedDetectQuest
+{
+    public static class GameData
+    {
+        public static Player CurrentPlayer {  get; set; }
+    }
+}
---
+++ DetectiveGameLogic/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBasedDetectQuest
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string CharacterTrait { get; set; }
+        public string InventorySlot { get; set; }
+        public string Contact { get; set; }
+    }
+}
